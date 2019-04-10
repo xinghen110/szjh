7e8f817568d5ef7956f4d488d8061e8a94ac62b7
root <whatreally@163.com>
2019-04-10 15:06:18 +0800

    审批日志相关数据库操作service实现

---
+++ src/main/java/com/magustek/szjh/approval/service/impl/ApprovalLogServiceImpl.java
@@ -0,0 +1,22 @@
+package com.magustek.szjh.approval.service.impl;
+
+import com.magustek.szjh.approval.bean.ApprovalLog;
+import com.magustek.szjh.approval.dao.ApprovalLogDAO;
+import com.magustek.szjh.approval.service.ApprovalLogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("ApprovalLogServiceImpl")
+public class ApprovalLogServiceImpl implements ApprovalLogService {
+    private final ApprovalLogDAO approvalLogDao;
+
+    public ApprovalLogServiceImpl(ApprovalLogDAO approvalLogDao) {
+        this.approvalLogDao = approvalLogDao;
+    }
+
+    @Override
+    public List<ApprovalLog> getAllLogsByHeaderid(Long headerid) {
+        return approvalLogDao.findAllByHeaderidOrderByIdAsc(headerid);
+    }
+}
